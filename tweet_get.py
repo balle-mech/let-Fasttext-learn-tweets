cef4cf20689b1b488875274683be87833f1445ff
a2c6201 <a2c6201@gmail.com>
2022-09-08 17:18:18 +0900

    学習データとテストデータに分けて3万件のツイート保存機能追加

--- tweet_get.py
+++ tweet_get.py
@@ -1,10 +1,12 @@
 import re
 import tweepy
 import MeCab
+from time import sleep
 
-COUNT = 200
-KEYWORD = 'ゲーム'
-CLASS_LABEL = "__label__ゲーム"
+COUNT = 100    # ツイート取得数
+train_idx = 80    # トレーニングデータ%
+SET = 30    # セット数×100件のツイートを取得
+KEYWORDS = ['エンジニア', 'ゲーム', 'アニメ']
 
 # 認証に必要なキーとトークン
 API_KEY = 'IR2CAa7c3w5OqzilT5iCPIAbg'
@@ -21,42 +23,53 @@ def authTwitter():
 
 def main():
     tweets = get_tweet()      #ツイートを取得
-    separate = to_separate(tweets)     #ツイートを分かち書き
-    write_txt(separate)        #ツイートをファイルに書き込み
+    result = separate_tweet(tweets)     #ツイートを分かち書き
+    write_txt(result)        #ツイートをファイルに書き込み
 
 # TwitterからKEYWORDに関連するツイートを取得
 def get_tweet():
     api = authTwitter() # 認証
-    tweets = api.search_tweets(q=KEYWORD,count=COUNT)
+    tweets = api.search_tweets(q=KEYWORD, count=COUNT)
     return tweets
 
 # 正規表現を使ってツイートから不要な情報を削除
 def format_text(text):
     text=re.sub(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-…]+', "", text)  # 外部リンクURL
     text=re.sub(r'@[\w/:%#\$&\?\(\)~\.=\+\-…]+', "", text)  #リプライ先のユーザーID
-    text=re.sub(r'＼', "", text)  
+    text=re.sub(r'＼', "", text)
     text=re.sub('／', "", text)
     text=re.sub('RT', "", text)
     text=re.sub('\n', "", text) # 改行文字
     return text
 
 # 文書を分かち書きし単語単位に分割
-def to_separate(tweets):
+def separate_tweet(tweets):
     results = []
     tagger = MeCab.Tagger('-Owakati')
 
     for tweet in tweets:
         content = format_text(tweet.text)
         wakati = tagger.parse(content)
-        results.append(' , '.join([CLASS_LABEL, wakati])) # ツイートごとにラベルを付与
+        results.append(' , '.join(['__label__' + KEYWORD, wakati])) # ツイートごとにラベルを付与
     return results
 
-# 学習モデル用のテキストファイルを作成    
+# 学習モデル用と評価用のテキストファイルを作成
 def write_txt(results):
-    with open(CLASS_LABEL + '.txt', 'a') as f:
+    idx = 0
+    with open('train_' + KEYWORD + '.txt', 'a') as f_train,\
+        open('test' + KEYWORD + '.txt', 'a') as f_test:
         for result in results:
-            f.write(result)
-        print(str(len(results))+"行を書き込み")
+            if idx < train_idx:
+                f_train.write(result)
+            else:
+                f_test.write(result)
+            idx += 1
+
+        print("{}ツイート100件取得{}セット目".format(KEYWORD, i))
 
 if __name__ == '__main__':
-    main()
+    for i in range(SET):
+        for KEYWORD in KEYWORDS:
+            main()
+        print('15秒待機')
+        sleep(15)
